1e8fec6cb0ae52e3a6d043f508bda41c0972dcee
jerom.park <jerom.park@kakaomobility.com>
2021-06-13 18:05:50 +0900

    add post-table & ui- update

--- src/main/java/com/web/pkl/PostController.java
+++ src/main/java/com/web/pkl/PostController.java
@@ -1,5 +1,7 @@
 package com.web.pkl;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import com.web.pkl.model.Post;
 import com.web.pkl.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,12 +9,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class PostController {
 
   @Autowired
   private PostRepository postRepository;
+
+  @RequestMapping(value="/posts", method = RequestMethod.GET)
+  @ResponseBody
+  public String posts(){
+    JSONObject obj = new JSONObject();
+
+    List<Post> posts = postRepository.findAll();
+    JSONArray GroupList = new JSONArray();
+    posts.forEach(
+        post -> GroupList.add(Arrays.asList(new String[]{post.getTitle(), post.getDescription()}))
+    );
+    obj.put("data", GroupList);
+    return obj.toString();
+  }
 
   @RequestMapping(value="/post", method = RequestMethod.POST)
   public String postNew(
